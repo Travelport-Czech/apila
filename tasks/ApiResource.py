b4e1f9e057b45cb50de31346d7d913a2fb304155
Tomas Srb <tomas.srb@travelportgds.cz>
2016-10-17 17:20:14 +0200

    Po prirazeni lambdy methode je treba nastavit pravo

--- tasks/ApiResource.py
+++ tasks/ApiResource.py
@@ -2,6 +2,7 @@ from Task import Task
 import bototools
 import botocore
 import name_constructor
+import uuid
 
 class ApiResource(Task):
   """Create resource and method on Api Gateway"""
@@ -24,7 +25,8 @@ class ApiResource(Task):
   def run(self, clients, cache):
     api_name = name_constructor.api_name(self.params['api'], self.config['user'], self.config['branch'])
     lambda_name = name_constructor.lambda_name(self.params['lambda'], self.config['user'], self.config['branch'])
-    lambda_arn = self.get_lambda_arn(clients.get('lambda'), cache, lambda_name)
+    lambda_params = self.get_lambda_params(clients.get('lambda'), cache, lambda_name, self.params['path'], self.params['method'])
+    lambda_arn = self.get_lambda_arn(lambda_params)
     if lambda_arn is None:
       return (False, "Lambda function '%s' not found" % lambda_name)
     path = self.params['path']
@@ -48,7 +50,9 @@ class ApiResource(Task):
       method_info = client.get_method(restApiId=api_id, resourceId=path_id, httpMethod=self.params['method'])
 #{u'apiKeyRequired': False, u'httpMethod': u'POST', u'methodIntegration': {u'integrationResponses': {u'200': {u'responseTemplates': {u'application/json': None}, u'statusCode': u'200'}}, u'passthroughBehavior': u'WHEN_NO_MATCH', u'cacheKeyParameters': [], u'uri': u'arn:aws:apigateway:eu-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:eu-central-1:860303221267:function:main_srbt_afd_attemptEvaluate/invocations', u'httpMethod': u'POST', u'cacheNamespace': u'ez205t', u'type': u'AWS'}, u'requestParameters': {}, 'ResponseMetadata': {'RetryAttempts': 0, 'HTTPStatusCode': 200, 'RequestId': '7b577a6b-913a-11e6-afe9-53022ccbd313', 'HTTPHeaders': {'x-amzn-requestid': '7b577a6b-913a-11e6-afe9-53022ccbd313', 'date': 'Thu, 13 Oct 2016 11:45:04 GMT', 'content-length': '594', 'content-type': 'application/json'}}, u'methodResponses': {u'200': {u'responseModels': {u'application/json': u'Empty'}, u'statusCode': u'200'}}, u'authorizationType': u'NONE'}
     except botocore.exceptions.ClientError:
+      permissions_arn = self.get_permission_arn(api_id, lambda_params)
       self.create_method(client, api_id, path_id, lambda_arn)
+      self.add_permission(clients.get('lambda'), lambda_name, permissions_arn, uuid.uuid4().hex)
       return (True, self.CREATED)
     if 'methodIntegration' not in method_info:
       self.create_integration(client, api_id, path_id, lambda_arn)
@@ -69,7 +73,7 @@ class ApiResource(Task):
       result = self.CHANGED
     return (True, result)
 
-  def get_lambda_arn(self, client, cache, lambda_name):
+  def get_lambda_params(self, client, cache, lambda_name, path, method):
     lambda_arn = cache.get('lambda', lambda_name)
     if lambda_arn is None:
       try:
@@ -78,16 +82,26 @@ class ApiResource(Task):
         return None
       lambda_arn = function_conf['FunctionArn']
       cache.put('lambda', lambda_name, lambda_arn)
+    parts = lambda_arn.split(':')
+    return {
+      'region': parts[3],
+      'acct_id': parts[4],
+      'version': client.meta.service_model.api_version,
+      'lambda_arn': lambda_arn,
+      'path': path,
+      'method': method
+    }
+
+  def get_lambda_arn(self, lambda_params):
 # arn:aws:apigateway:eu-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:eu-central-1:860303221267:function:main_srbt_afd_attemptEvaluate/invocations
 # arn:aws:lambda:eu-central-1:860303221267:function:main_srbt_afd_attemptEvaluate
-    parts = lambda_arn.split(':')
-    lambda_uri = 'arn:aws:apigateway:%(region)s:lambda:path/%(version)s/functions/%(lambda_arn)s/invocations' % {
-      'region': parts[3],
-      'version': client.meta.service_model.api_version,
-      'lambda_arn': lambda_arn
-    }
+    lambda_uri = 'arn:aws:apigateway:%(region)s:lambda:path/%(version)s/functions/%(lambda_arn)s/invocations' % lambda_params
     return lambda_uri
 
+  def get_permission_arn(self, api_id, lambda_params):
+# "arn:aws:execute-api:{aws-region}:{aws-acct-id}:{aws-api-id}/*/POST/{lambda-function-name}"
+    lambda_params['api_id'] = api_id
+    return 'arn:aws:execute-api:%(region)s:%(acct_id)s:%(api_id)s/*/%(method)s%(path)s' % lambda_params
 
   def create_method(self, client, api_id, path_id, lambda_arn):
     client.put_method(restApiId=api_id, resourceId=path_id, httpMethod=self.params['method'], authorizationType='NONE')
@@ -96,3 +110,5 @@ class ApiResource(Task):
   def create_integration(self, client, api_id, path_id, lambda_arn):
     client.put_integration(restApiId=api_id, resourceId=path_id, httpMethod=self.params['method'], type='AWS', uri=lambda_arn, integrationHttpMethod=self.params['method'])
     
+  def add_permission(self, client, lambda_name, permissions_arn, uuid):
+    result = client.add_permission(FunctionName=lambda_name, StatementId=uuid, Action="lambda:InvokeFunction", Principal="apigateway.amazonaws.com", SourceArn=permissions_arn)
