428fe2e41fc36e3c4d6de08e91733151d06ebecb
Vít Slaninka <vit.slaninka@idc.cz>
2017-11-24 09:58:17 +0100

    Added enabling CORS in 5XX and 4XX api gateway responses

--- tasks/ApiTask.py
+++ tasks/ApiTask.py
@@ -5,7 +5,8 @@ class ApiTask(Task):
   """Create an api on API gateway"""
   known_params = {
     'name': 'name of the api (will be concatenated with fields branch and user from config.yml)',
-    'description': 'short human readable description of the api'
+    'description': 'short human readable description of the api',
+    'cors' : 'add header Access-Control-Allow-Origin : * to 5XX and 4XX gateway responses'
   }
   required_params = ('name',)
   required_configs = ('user', 'branch')
@@ -28,7 +29,18 @@ class ApiTask(Task):
       result = self.CREATED
     elif api['description'] != api_description:
       client.update_rest_api(restApiId=api['id'], patchOperations=[{'op': 'replace', 'path': '/description', 'value': api_description}])
-      result = self.CHANGED
+      result = self.CHANGED      
+    if 'cors' in self.params:
+      errResponse4 = client.get_gateway_response(restApiId=api['id'], responseType='DEFAULT_4XX')
+      errResponse5 = client.get_gateway_response(restApiId=api['id'], responseType='DEFAULT_5XX')
+      if self.params['cors'] == True:
+        client.update_gateway_response(restApiId=api['id'], responseType='DEFAULT_4XX', patchOperations=[{'op': 'add', 'path': '/responseParameters/gatewayresponse.header.Access-Control-Allow-Origin', 'value': "'*'"}])
+        client.update_gateway_response(restApiId=api['id'], responseType='DEFAULT_5XX', patchOperations=[{'op': 'add', 'path': '/responseParameters/gatewayresponse.header.Access-Control-Allow-Origin', 'value': "'*'"}])
+        result = self.CHANGED      
+      elif self.params['cors'] == False:
+        client.update_gateway_response(restApiId=api['id'], responseType='DEFAULT_4XX', patchOperations=[{'op': 'remove', 'path': '/responseParameters/gatewayresponse.header.Access-Control-Allow-Origin'}])
+        client.update_gateway_response(restApiId=api['id'], responseType='DEFAULT_5XX', patchOperations=[{'op': 'remove', 'path': '/responseParameters/gatewayresponse.header.Access-Control-Allow-Origin'}])
+        result = self.CHANGED        
     cache.put('api', api['name'], api['id'])
     return (True, result)
 
